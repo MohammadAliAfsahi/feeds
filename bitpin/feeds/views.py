76f14b555737eccf6bc8c03fec65051fccb2ace0
MohammadAliAfsahi <35253602+MohammadAliAfsahi@users.noreply.github.com>
2022-04-12 22:57:19 +0430

    Update View

--- bitpin/feeds/views.py
+++ bitpin/feeds/views.py
@@ -21,3 +21,4 @@ class UserRateView(ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+# This is a test
